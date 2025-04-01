425c4aa37efb60cde023abdf13350976cd69d60e
María Fernández <matefe1706@gmail.com>
2025-04-01 12:30:48 +0200

    feat(components): home and catalogo pages added and routed

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <nav>
+      <NuxtLink to="/catalogo">Catalogo</NuxtLink>
+    </nav>
+    <h1>Home</h1>
+  </header>
+  
+</template>
+
+<style lang="scss" scoped></style>
